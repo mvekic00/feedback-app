26321822290ab6744572d07ff5d51c3422f75ac3
mvekic00 <mvekic00@fesb.hr>
2022-04-05 19:15:25 +0200

    feedback item,list and data

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a risus eget nulla finibus mattis nec vitae odio. Donec ut.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a risus eget nulla finibus mattis nec vitae odio. Donec ut.",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a risus eget nulla finibus mattis nec vitae odio. Donec ut.",
+  },
+];
+
+export default FeedbackData
