f874d43e34cfb6a34acf1f6efc1805e449ac772c
mvekic00 <mvekic00@fesb.hr>
2022-04-06 10:57:18 +0200

    Delete feedback

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback,handleDelete_list}) {
     if(!feedback || feedback.length === 0){
         return <p>No Feedback Yet</p>
     }
 
   return (
     <div className='feedback-list'>
-      {feedback.map((item)=>(
-          <FeedbackItem key={item.id} item={item}/>
+      {feedback.map((feed)=>(
+          <FeedbackItem key={feed.id} item={feed} handleDelete={handleDelete_list}/>
       ))}
     </div>
   )
