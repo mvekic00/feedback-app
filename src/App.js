8b4d5fb58d3d9be8a31a6cc3adf260cd76064a07
mvekic00 <mvekic00@fesb.hr>
2022-04-07 16:42:40 +0200

    adding_feedback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
@@ -15,11 +16,16 @@ function App() {
         }
     }
 
+    const addFeedback = (newFeedback) =>{
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback,...feedback])
+    }
+
     return ( 
         <>
         <Header/>
         <div className="container">
-            <FeedbackForm/>
+            <FeedbackForm handleAdd={addFeedback}/>
             <FeedbackStats feedback={feedback}/>
             <FeedbackList feedback={feedback} handleDeleteinlist = {deleteFeedback}/>
         </div>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
@@ -7,13 +8,25 @@ function FeedbackList({feedback,handleDeleteinlist}) {
         return <p>No Feedback Yet</p>
     }
 
-  return (
+    return (
+      <div className='feedback-list'>
+        <AnimatePresence>
+        {feedback.map((feed)=>(
+            <motion.div key={feed.id} initial={{opacity: 0}} animate={{opacity:1}} exit={{opacity:0}}>
+              <FeedbackItem key={feed.id} item={feed} handleDelete={handleDeleteinlist}/>
+            </motion.div>
+        ))}
+        </AnimatePresence>
+      </div>
+    )
+
+/*   return (
     <div className='feedback-list'>
       {feedback.map((feed)=>(
           <FeedbackItem key={feed.id} item={feed} handleDelete={handleDeleteinlist}/>
       ))}
     </div>
-  )
+  ) */
 }
 
 FeedbackList.propTypes ={
